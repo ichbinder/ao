7e8f6af4f986ca6ab94050a9bba43d30aa57cf0f
Jakob Warnow <jakob.warnow@gmx.de>
2015-11-04 20:19:51 +0100

    Jakob: Aufgabe  5 Start

---
+++ AO/src/ue5/HeapSort.java
@@ -0,0 +1,21 @@
+package ue5;
+
+public class HeapSort {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public HeapSort() {
+		int[] list = {2, 6, 8, 43, 24, 12, 23, 46, 99, 665};
+		
+	}
+	
+	private int sort(int[] list) {
+		
+		
+		
+		return list[0];
+	}
+
+}
