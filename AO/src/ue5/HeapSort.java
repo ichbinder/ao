c02d8937a029c7fb308340f4be1e65478082fa6c
Jakob Warnow <jakob.warnow@gmx.de>
2015-11-14 07:30:59 +0100

    Jakob: add to index

--- AO/src/ue5/HeapSort.java
+++ AO/src/ue5/HeapSort.java
@@ -1,21 +1,53 @@
 package ue5;
 
 public class HeapSort {
-
-	public static void main(String[] args) {
-
+	
+	public static void heapsort(int[] A) {
+		buildMaxHeap(A);
+		
+		for (int i = A.length -1; i > 0; --i) {
+			swap(A, 0, i);
+			maxHeapify(A, i, A.length -1);
+		}
 	}
 	
-	public HeapSort() {
-		int[] list = {2, 6, 8, 43, 24, 12, 23, 46, 99, 665};
+	private static void maxHeapify(int[] A, int i, int heapSize) {
+		int l = left(i);
+		int r = right(i);
+		int maximum;
 		
+		if (l < heapSize && A[l] > A[i])
+			maximum = l;
+		else 
+			maximum = i;
+		
+		if (r < heapSize && A[r] > A[maximum])
+			maximum = r;
+		
+		if (maximum != i) {
+			swap (A, i, maximum);
+			maxHeapify(A, maximum, heapSize);
+		}
 	}
 	
-	private int sort(int[] list) {
-		
-		
-		
-		return list[0];
+	private static void buildMaxHeap(int[] A) {
+		for (int i = (A.length / 2); i >= 0; --i) {
+			maxHeapify(A, i, A.length);
+		}
 	}
-
+	
+	private static void swap(int[] A, int i, int childIndex) {
+		int temp = A[i];
+		A[i] = A[childIndex];
+		A[childIndex] = temp;
+	}
+	
+	private static int left (int i) {
+		return 2 * i;
+	}
+	
+	private static int right (int i) {
+		return 2 * i + 1;
+	}
 }
+
