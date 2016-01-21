0587e44d9ae0fc4ec0e2e70ad0996b753423d291
jakob <jakob@jakob-vm01>
2016-01-21 13:28:21 +0100

    Jakob: die LCS vertiggestellt, es muss noch ein wehnig feinschif gemacht werden

---
+++ AO/src/ue10/LCS.java
@@ -0,0 +1,45 @@
+package ue10;
+
+public class LCS {
+	private float c[][] = {};
+	private String b[][] = {};
+
+	public static void lengthLCS(float X[], float Y[]) {
+		int m = X.length;
+		int n = Y.length;
+		float c[][] = {};
+		String b[][] = {};
+		for (int i = 0; i < m; i++)
+			c[i][0] = 0;
+		for (int j = 0; j < n; j++)
+			c[0][j] = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (X[i] == Y[j]) {
+					c[i][j] = c[i-1][j-1] + 1;
+					b[i][j] = "links oben";
+				} else if (c[i-1][j] >= c[i][j-1]) {
+					c[i][j] = c[i-1][j];
+					b[i][j] = "oben";
+				} else {
+					c[i][j] = c[i][j-1];
+					b[i][j] = "links";
+				}
+			}
+		}	
+	}
+	
+	public static float printLCS(String b[][], float X[], int i, int j) {
+		if (i == 0 || j == 0)
+			return 0.0f;
+		if (b[i][j] == "links oben"){
+			printLCS(b, X, i-1, j-1);
+			System.out.println(X[i]);
+		} else if (b[i][j] == "oben") {
+			printLCS(b, X, i-1, j);
+		} else {
+			printLCS(b, X, i, j-1);
+		}
+		return 0.0f;
+	}
+}
